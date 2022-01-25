996f5ed4cd9bf17f82d86daaa0da0fa017bf4bf1
Yonas Yeneneh <expertsanoy@gmail.com>
2022-01-25 15:14:31 -0500

    update on school api documentation

--- src/routers/school.js
+++ src/routers/school.js
@@ -32,18 +32,61 @@ const {
  *       properties:
  *         id:
  *           type: int
- *           description: The unique id of the country
- *         code:
+ *           description: The unique id of the school
+ *         school_id:
  *           type: string
- *           description: country code
+ *           description: The school_id provided by government
  *         name:
  *           type: string
+ *           description: school name
+ *         address:
+ *           type: string
+ *           description: school address
+ *         postal_code:
+ *           type: string
+ *           description: school postal_code
+ *         email:
+ *           type: string
+ *           description: school email
+ *         education_level:
+ *           type: string
+ *           description: school education_level
+ *         environment:
+ *           type: string
+ *           description: school environment
+ *         school_type:
+ *           type: string
+ *           description: school type
+ *         country_id:
+ *           type: int
+ *           description: country id
+ *         country:
+ *           type: string
  *           description: country name
+ *         location_id:
+ *           type: int
+ *           description: school location_id
+ *         admin_2_name:
+ *           type: string
+ *           description: admin_2_name
+ *         admin_3_name:
+ *           type: string
+ *           description: admin_3_name
+ *         admin_4_name:
+ *           type: string
+ *           description: admin_4_name
+ *         giga_id:
+ *           type: string
+ *           description: A unique giga_id
  *       example:
  *         id: 144
  *         code: BR
- *         name: Brazil
+ *         name: C.B.T.A. NUM. 36 EMILIANO ZAPA
+ *         country_id: 146,
+ *         country: Mexico
+ *         giga_id: 7b995921-6531-4eaf-a399-57491f5de640 *         
  */
+
 
 /**
  * @swagger

0e025c7c57faabb8ae338c37cb9bcc70066eae7e
Yonas Yeneneh <expertsanoy@gmail.com>
2022-01-25 09:07:15 -0500

    update

--- src/db.js
+++ src/db.js
@@ -5,9 +5,11 @@ require("dotenv").config();
 module.exports = knex({
   client: 'postgres',
   connection: {
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
+    host: 'docker',
+    user: '123456',
+    password: 'db',
+    database: 'postgres',
   },
 })
+
+
